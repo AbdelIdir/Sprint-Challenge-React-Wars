04aefcc28581267c2b53490e943d2477910a997a
AbdelIdir <homeworkoracle@protonmail.com>
2019-09-15 22:51:19 +0200

    added flex wrap,turned the data into single individual cards,added opacity to the cards background;added conditional operators for the heights,genders

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,51 +2,62 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Character from "./components/Character";
+import styled from "styled-components";
 
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
 
+// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
+const StyleApp = styled.div`
+  display:flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+  color: black;
+  border: 5px solid black;
+  height: 100%;
+`;
 
+function App() {
+  const [chars, setChars] = useState([]);
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/").then(responseApi => {
+      const res = responseApi.data.results;
+      console.log(res);
+      setChars(res);
+    });
+  }, []);
+  return (
+    <StyleApp>
+      {chars.map((item, index) => {
+        return (
+          <Character
+            key={index}
+            CharacterObject={item}
 
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-
-  
-  function App () {
-   const [chars, setChars] = useState ([]);
-   useEffect(() => {
-       axios.get("https://swapi.co/api/people/")
-       .then(responseApi => {
-           const res = responseApi.data.results
-           console.log(res);
-           setChars(res);
-       })
-   },[])
-return (
-   <div>
-       
-       
-
-
-           {chars.map(item=>{
-               return(
-                   <Character
-                   name ={item.name}
-                   height={item.height}
-                   gender={item.gender}
-                   mass={item.mass}
-                   skinColor={item.skin_color}
-                   />
-               )
-           })}
-       
-   </div>
-)
+            //  name ={item.name}
+            //  height={item.height}
+            //  gender={item.gender}
+            //  mass={item.mass}
+            //  skinCo={item.skin_color}
+          />
+        );
+      })}
+    </StyleApp>
+  );
 }
 
-
 export default App;
+
+// return(
+//   <Character
+//   key = {index}
+// CharacterObject={item}
+//   />
+// )
+// })}
+// </StyleApp>
+
+// If I use this,Ill have to change  ittem to characterobject withint const{}= props
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,25 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 const StyleC1 = styled.div`
-  background: blue;
+  background: rgba(0, 0, 0, 0.5);
   color: black;
+  font-weight: bold;
+  margin: 10px;
+  padding: 5px;
+  width: 210px;
+  height: 210px;
 `;
+
 function Character(props) {
-  const { item } = props;
-  console.log(item);
+  const { CharacterObject } = props;
+  console.log(CharacterObject);
   return (
     <StyleC1>
-      {" "}
-      <div>
-        
-        {` Name : ${props.name} 
-                   Height : ${props.height}
-                   Gender : ${props.gender}
-                   Mass : ${props.mass}
-                     SkinColor : ${props.skinColor}`} 
-      </div>
+      <p>Name : {CharacterObject.name} </p>
+      <p>
+        {" "}
+        Height :{" "}
+        {CharacterObject.height > 160 &&
+          " That's a tall person => " + CharacterObject.height + " cm "}
+        {CharacterObject.height < 160 &&
+          " That's a short person => " + CharacterObject.height + " cm "}
+      </p>
+      <p>
+        Gender : {CharacterObject.gender === "n/a" && "It's a robot "}
+        {CharacterObject.gender === "male" && "It's a man"}
+        {CharacterObject.gender === "female" && "It's a woman "}
+      </p>
+      <p>Mass : {CharacterObject.mass}</p>
+      <p>SkinColor : {CharacterObject.skin_color}</p>
     </StyleC1>
   );
 }
 
 export default Character;
+
+// <StyleC1>
+//       {" "}
+//       <div>
+
+//         {` Name : ${props.name}
+//                    Height : ${item.height}
+//                    Gender : ${item.gender}
+//                    Mass : ${item.mass}
+//                      SkinColor : ${item.skinCo}`}
+//       </div>
+//     </StyleC1>
+//   );
+// }
